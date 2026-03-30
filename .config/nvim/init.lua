46cc81a258705fbda74ec2ce329670aace564de3
QUIRINO Paulo <paulo.quirino@vinci-energies.net>
2026-03-30 13:32:47 +0100

    Work updates

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -19,6 +19,7 @@ vim.opt.fileformats = { "unix", "dos" }
 vim.opt.fileformat = "unix"
 
 local USE_COPILOT_COMPLETION = true
+local USE_TELESCOPE = true
 
 -- }}}
 -- self-contained plugin system {{{
@@ -123,15 +124,19 @@ local function notify(msg, level)
   vim.notify(msg, level or vim.log.levels.INFO, { title = "init.lua" })
 end
 
-local function nvim_at_least(major, minor, patch)
+local function nvim_version_number()
   local v = vim.version()
-  local cur = (v.major * 10000) + (v.minor * 100) + (v.patch or 0)
-  local req = (major * 10000) + (minor * 100) + (patch or 0)
-  return cur >= req
+  return (v.major * 10000) + (v.minor * 100) + (v.patch or 0)
+end
+
+local NVIM = nvim_version_number()
+
+local function has_nvim(major, minor, patch)
+  return NVIM >= ((major * 10000) + (minor * 100) + (patch or 0))
 end
 
 local function require_nvim(major, minor, patch, msg)
-  if nvim_at_least(major, minor, patch) then
+  if has_nvim(major, minor, patch) then
     return true
   end
 
@@ -307,47 +312,57 @@ end
 --- }}}
 -- lsp stuff {{{
 -- lsp mappings {{{
-local on_attach = function(_, bufnr)
-  local function bufmap(mode, lhs, rhs, desc)
-    vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = desc })
-  end
-
-  bufmap("n", "gd", vim.lsp.buf.definition, "LSP definition")
-  bufmap("n", "gD", vim.lsp.buf.declaration, "LSP declaration")
-  bufmap("n", "gi", vim.lsp.buf.implementation, "LSP implementation")
-  bufmap("n", "gr", vim.lsp.buf.references, "LSP references")
-  bufmap("n", "K", vim.lsp.buf.hover, "LSP hover")
-  bufmap("n", "<leader>lr", vim.lsp.buf.rename, "LSP rename")
-  bufmap({ "n", "x" }, "<leader>la", vim.lsp.buf.code_action, "LSP code action")
-  bufmap("n", "<leader>lf", function()
-    vim.lsp.buf.format({ async = true })
-  end, "LSP format")
+local function lsp_bufmap(bufnr, mode, lhs, rhs, desc)
+  vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = desc })
 end
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    local bufnr = args.buf
+
+    lsp_bufmap(bufnr, "n", "gd", vim.lsp.buf.definition, "LSP definition")
+    lsp_bufmap(bufnr, "n", "gD", vim.lsp.buf.declaration, "LSP declaration")
+    lsp_bufmap(bufnr, "n", "gi", vim.lsp.buf.implementation, "LSP implementation")
+    lsp_bufmap(bufnr, "n", "gr", vim.lsp.buf.references, "LSP references")
+    lsp_bufmap(bufnr, "n", "K", vim.lsp.buf.hover, "LSP hover")
+    lsp_bufmap(bufnr, "n", "<leader>lr", vim.lsp.buf.rename, "LSP rename")
+    lsp_bufmap(bufnr, { "n", "x" }, "<leader>la", vim.lsp.buf.code_action, "LSP code action")
+    lsp_bufmap(bufnr, "n", "<leader>lf", function()
+      vim.lsp.buf.format({ async = true })
+    end, "LSP format")
+  end,
+})
 
 nmap("<leader>e", vim.diagnostic.open_float, "Line diagnostics")
 
-local ts_ls_cmd = require_executable("typescript-language-server", "ts_ls lsp configuration")
-local deno_cmd = require_executable("deno", "deno lsp configuration")
-local clangd_cmd = require_executable("clangd", "clangd lsp configuration")
+local ts_ls_cmd = first_executable("typescript-language-server")
+local deno_cmd = first_executable("deno")
+local clangd_cmd = first_executable("clangd")
 -- }}}
 -- lsp compat helper {{{
-local lspconfig_spec = nvim_at_least(0, 10, 0) and {} or { rev = "v0.1.8" }
+local lspconfig_spec = has_nvim(0, 10, 0) and {} or { rev = "v0.1.8" }
 
 use("neovim/nvim-lspconfig", lspconfig_spec)
 local has_new_lsp = type(vim.lsp.config) == "function"
-local lspconfig = not has_new_lsp and req("lspconfig") or nil
-local ts_server_name = has_new_lsp and "ts_ls" or "tsserver"
+local ts_server_name
+
+if has_nvim(0, 10, 0) then
+  ts_server_name = "ts_ls"
+else
+  ts_server_name = "tsserver"
+end
 
 local function setup_lsp(name, config)
-  if has_new_lsp then
+  if has_nvim(0, 10, 0) then
     vim.lsp.config(name, config)
     vim.lsp.enable(name)
     return true
-  end
-
-  if lspconfig and lspconfig[name] then
-    lspconfig[name].setup(config)
-    return true
+  else
+    local lspconfig = req("lspconfig")
+    if lspconfig and lspconfig[name] then
+      lspconfig[name].setup(config)
+      return true
+    end
   end
 
   notify("LSP setup failed for " .. name, vim.log.levels.WARN)
@@ -377,15 +392,15 @@ if deno_cmd then
       end
     end,
     capabilities = lsp_capabilities,
-    on_attach = on_attach,
+    -- on_attach = on_attach,
   })
 else
   notify("denols not enabled: missing deno", vim.log.levels.WARN)
 end
 -- }}}
--- typescript {{{
+-- typescript (ts_ls) {{{
 if ts_ls_cmd then
-  setup_lsp(ts_server_name, {
+  setup_lsp(ts_ls_cmd, {
     cmd = { ts_ls_cmd, "--stdio" },
     filetypes = {
       "typescript",
@@ -394,8 +409,8 @@ if ts_ls_cmd then
       "javascriptreact",
     },
     root_dir = function(bufnr, on_dir)
-      local deno_root = vim.fs.root(bufnr, { "deno.json", "deno.jsonc" })
-      if deno_root then
+      -- Skip if this is a Deno project
+      if vim.fs.root(bufnr, { "deno.json", "deno.jsonc" }) then
         return
       end
 
@@ -403,22 +418,17 @@ if ts_ls_cmd then
         "package.json",
         "tsconfig.json",
         "jsconfig.json",
+        "node_modules",
       })
-
       if root then
-        if has_new_lsp then
-          on_dir(root)
-        else
-          return root
-        end
+        on_dir(root) -- always call it on 0.12+
       end
     end,
     single_file_support = false,
     capabilities = lsp_capabilities,
-    on_attach = on_attach,
   })
 else
-  notify("ts_ls not enabled: missing typescript-language-server and/or node", vim.log.levels.WARN)
+  notify("ts_ls not enabled: missing typescript-language-server", vim.log.levels.WARN)
 end
 -- }}}
 -- clangd {{{
@@ -427,7 +437,7 @@ if clangd_cmd then
     cmd = { clangd_cmd },
     filetypes = { "c", "cpp", "objc", "objcpp", "cuda", "proto" },
     capabilities = lsp_capabilities,
-    on_attach = on_attach,
+    -- on_attach = on_attach,
   })
 else
   notify("clangd not enabled: missing clangd", vim.log.levels.WARN)
@@ -438,8 +448,6 @@ end
 -- find and grep {{{
 vim.opt.path = { ".", "**" }
 
-vim.keymap.set("n", "<leader>ff", ":find ", { desc = "Find files" })
-
 if vim.fn.executable("rg") == 1 then
   vim.opt.grepprg = "rg --vimgrep --smart-case"
   vim.opt.grepformat = "%f:%l:%c:%m"
@@ -447,46 +455,80 @@ else
   notify("ripgrep not found in PATH", vim.log.levels.WARN)
 end
 
-if vim.fn.executable("rg") == 1 then
-  vim.opt.grepprg = "rg --vimgrep --smart-case"
-  vim.opt.grepformat = "%f:%l:%c:%m"
+-- telescope {{{
+if USE_TELESCOPE and require_nvim(0, 9, 0, "loading telescope") then
+  use("nvim-lua/plenary.nvim")
 
-  nmap("<leader>fg", function()
-    local pat = vim.fn.input("Grep > ")
-    if pat == "" then
-      return
+  local telescope_spec = has_nvim(0, 10, 4) and {} or { rev = "0.1.9" }
+  use("nvim-telescope/telescope.nvim", telescope_spec)
+
+  local telescope = req("telescope")
+  if telescope then
+    telescope.setup({
+      -- minimal defaults (you can expand later if you want)
+      defaults = {
+        mappings = {
+          i = {
+            ["<C-j>"] = "move_selection_next",
+            ["<C-k>"] = "move_selection_previous",
+          },
+        },
+      },
+    })
+
+    local builtin = req("telescope.builtin")
+    if builtin then
+      nmap("<leader>ff", builtin.find_files, "Find files")
+      nmap("<leader>fg", builtin.live_grep,  "Live grep")
+      nmap("<leader>fb", builtin.buffers,    "Buffers")
+      nmap("<leader>fh", builtin.help_tags,  "Help tags")
     end
-    vim.cmd("grep! " .. vim.fn.shellescape(pat))
-    vim.cmd("copen")
-  end, "Grep files")
+  end
 else
-  nmap("<leader>fg", function()
-    local pat = vim.fn.input("Grep > ")
-    if pat == "" then
-      return
-    end
-    vim.cmd("silent vimgrep /" .. vim.fn.escape(pat, "/\\") .. "/gj **/*")
-    vim.cmd("copen")
-  end, "Grep files")
-end
+  vim.keymap.set("n", "<leader>ff", ":find ", { desc = "Find files" })
 
-nmap("<leader>fb", function()
-  local buffers = vim.fn.getbufinfo({ buflisted = 1 })
-  vim.ui.select(buffers, {
-    prompt = "Buffers",
-    format_item = function(buf)
-      return string.format("%d: %s", buf.bufnr, buf.name ~= "" and buf.name or "[No Name]")
-    end,
-  }, function(choice)
-    if choice then
-      vim.cmd("buffer " .. choice.bufnr)
-    end
-  end)
-end, "Buffers")
+  if vim.fn.executable("rg") == 1 then
+    vim.opt.grepprg = "rg --vimgrep --smart-case"
+    vim.opt.grepformat = "%f:%l:%c:%m"
+
+    nmap("<leader>fg", function()
+      local pat = vim.fn.input("Grep > ")
+      if pat == "" then
+        return
+      end
+      vim.cmd("grep! " .. vim.fn.shellescape(pat))
+      vim.cmd("copen")
+    end, "Grep files")
+  else
+    nmap("<leader>fg", function()
+      local pat = vim.fn.input("Grep > ")
+      if pat == "" then
+        return
+      end
+      vim.cmd("silent vimgrep /" .. vim.fn.escape(pat, "/\\") .. "/gj **/*")
+      vim.cmd("copen")
+    end, "Grep files")
+  end
+
+  nmap("<leader>fb", function()
+    local buffers = vim.fn.getbufinfo({ buflisted = 1 })
+    vim.ui.select(buffers, {
+      prompt = "Buffers",
+      format_item = function(buf)
+        return string.format("%d: %s", buf.bufnr, buf.name ~= "" and buf.name or "[No Name]")
+      end,
+    }, function(choice)
+      if choice then
+        vim.cmd("buffer " .. choice.bufnr)
+      end
+    end)
+  end, "Buffers")
+end
+-- }}}
 
 -- }}}
 -- gitsigns {{{
-local gitsigns_spec = nvim_at_least(0, 10, 0) and {} or { rev = "v1.0.2" }
+local gitsigns_spec = has_nvim(0, 10, 0) and {} or { rev = "v1.0.2" }
 use("lewis6991/gitsigns.nvim", gitsigns_spec)
 
 local gitsigns = req("gitsigns")
